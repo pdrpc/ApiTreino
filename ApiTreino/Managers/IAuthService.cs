136f24db32a28f97b603def3e693256598ecb1bf
pdrpc <pdr.pradocamargo@gmail.com>
2021-04-12 10:42:09 -0300

    Commitng managers

---
+++ ApiTreino/Managers/IAuthService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using ApiTreino.Models;
+
+namespace ApiTreino.Managers
+{
+    public interface IAuthService
+    {
+        string SecretKey { set; get; }
+
+        bool IsTokenValid(string token);
+
+        string GenerateToken(UserInfoViewModel model);
+
+        IEnumerable<Claim> GetTokenClaims(string token);
+    }
+}
---
+++ ApiTreino/Managers/JWTService.cs
@@ -0,0 +1,111 @@
+﻿using ApiTreino.Models;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+
+namespace ApiTreino.Managers
+{
+    public class JWTService : IAuthService
+    {
+        #region Members
+        public string SecretKey { get; set;}
+        #endregion
+
+        #region Constructor
+        public JWTService(string secretKey)
+        {
+            SecretKey = secretKey;
+        }
+        #endregion
+
+        #region Private Methods
+        private SecurityKey GetSymmetricSecurityKey() {
+            byte[] symmetricKey = Convert.FromBase64String(SecretKey);
+            return new SymmetricSecurityKey(symmetricKey);
+        }
+
+        private TokenValidationParameters GetTokenValidationParameters() {
+            return new TokenValidationParameters()
+            {
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                IssuerSigningKey = GetSymmetricSecurityKey()
+            };
+        }
+        #endregion
+
+        #region Public Methods
+        public string GenerateToken(UserInfoViewModel model)
+        {
+            if (model == null || model.Claims == null || model.Claims.Length == 0) 
+            {
+                throw new ArgumentException("Arguments to create token are not valid.");
+            }
+
+            SecurityTokenDescriptor securityTokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(model.Claims),
+                Expires = DateTime.UtcNow.AddMinutes(Convert.ToInt32(model.ExpireMinutes)),
+                SigningCredentials = new SigningCredentials(GetSymmetricSecurityKey(), model.SecurityAlgorithm)
+            };
+
+            JwtSecurityTokenHandler jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+            SecurityToken securityToken = jwtSecurityTokenHandler.CreateToken(securityTokenDescriptor);
+
+            string token = jwtSecurityTokenHandler.WriteToken(securityToken);
+
+            return token;
+        }
+
+        public IEnumerable<Claim> GetTokenClaims(string token)
+        {
+            if (string.IsNullOrEmpty(token)) 
+            {
+                throw new ArgumentException("Given Token is null or empty");
+            }
+
+            TokenValidationParameters tokenValidationParameters = GetTokenValidationParameters();
+            JwtSecurityTokenHandler jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+
+
+            try
+            {
+                ClaimsPrincipal tokenValid = jwtSecurityTokenHandler.ValidateToken(token, tokenValidationParameters, out SecurityToken validatedToken);
+
+                return tokenValid.Claims;
+            }
+            catch(Exception e) {
+                throw e;
+            }
+        }
+
+
+        public bool IsTokenValid(string token)
+        {
+            if (string.IsNullOrEmpty(token))
+            {
+                throw new ArgumentException("Given Token is null or empty");
+            }
+
+            TokenValidationParameters tokenValidationParameters = GetTokenValidationParameters();
+            JwtSecurityTokenHandler jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+
+
+            try
+            {
+                ClaimsPrincipal tokenValid = jwtSecurityTokenHandler.ValidateToken(token, tokenValidationParameters, out SecurityToken validatedToken);
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        #endregion
+    }
+}
